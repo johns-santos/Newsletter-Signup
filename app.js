2f2b50c3f1669b17552cc03efd4d781a06e21d20
johns-santos <johnsanthony.santos@gmail.com>
2021-12-30 23:44:45 -0600

    Newsletter Sign Up app completed.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 const app = express();
 
 
@@ -16,11 +17,55 @@ app.get('/', function (req, res) {
   })
 
 app.post("/", function(req,res){
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.Email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.Email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
 
-    console.log(firstName, lastName, email);
+
+const jsonData = JSON.stringify(data);
+
+const url = "https://us20.api.mailchimp.com/3.0/lists/6f140249cf";
+
+const options = {
+    method: "POST",
+    auth: "<apikey.txt>"
+
+}
+
+const request = https.request(url, options, function(response){
+    if(response.statusCode == 200) {
+        res.sendFile(__dirname + '/success.html')
+    } else {
+        res.sendFile(__dirname + '/failure.html')
+
+    }
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+        
+    });
+});
+
+request.write(jsonData);
+
+request.end();
+
+});
+
+// FAILURE ROUTE - Upon failure redirect to "/"
+app.post("/failure", function(req, res){
+    res.redirect("/")
 
 })
 
@@ -28,8 +73,5 @@ app.post("/", function(req,res){
 app.listen(3000, function(){
     console.log('Server running on port 3000')
 })
-
-
-
 
 
